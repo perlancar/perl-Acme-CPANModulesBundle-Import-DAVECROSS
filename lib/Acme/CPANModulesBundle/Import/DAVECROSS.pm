6884736c150c3e615b62e5ed74b7b70ccc68c1ad
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2021-11-17 20:27:17 +0700

    Initial commit

---
+++ lib/Acme/CPANModulesBundle/Import/DAVECROSS.pm
@@ -0,0 +1,13 @@
+package Acme::CPANModulesBundle::Import::DAVECROSS;
+
+use strict;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+1;
+# ABSTRACT: Acme::CPANModules::* created from DAVECROSS's posts
+
+=head1 DESCRIPTION
